b0271e573438cc117efd8f24db14c65636a6c3bd
jjw213 <ssvsxx@naver.com>
2022-10-28 18:59:46 +0900

    완성 (주석 정리)

--- src/Netmarble2022ct.java
+++ src/Netmarble2022ct.java
@@ -23,19 +23,18 @@ import java.util.StringTokenizer;
  * logs의 원소들은 날짜순으로 정렬되어있음
  * 이미 체크되어 있는 미션을 또 성공하진 않음
  */
-
+// 예제 입력값
+// 1 1 1 2 1 3 1 4 1 5 1 6 1 7 1 8 1 9 1 10 15 19 15 9 15 10 15 21 15 25 15 1 15 27 15 13 24 5 24 16 24 23
+// 출력값 : 10 (일간 빙고 8점, 월간 빙고 2점)
 public class Netmarble2022ct {
     static int[][] daily = new int[3][3];
     static int[][] weekly = new int[3][3];
     static int[][] monthly = new int[3][3];
     static int bingo = 0;
     public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner(System.in);
-//        a= new int[sc.nextInt()];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[][] logs = new int[st.countTokens() / 2][2];
-//        System.out.println(st.countTokens());
         int i = 0;
         while (st.hasMoreTokens()) {
             int n = Integer.parseInt(st.nextToken());
@@ -43,9 +42,6 @@ public class Netmarble2022ct {
             logs[i][0] = n;
             logs[i++][1] = m;
         }
-//        for(int j=0;j<logs.length;j++){
-//            System.out.println(logs[j][1]);
-//        }
         solution(logs);
     }
 
@@ -54,23 +50,12 @@ public class Netmarble2022ct {
         for (int i = 0; i < logs.length; i++) { //빙고 채우기
             if (logs[i][0] != day) {
                 //날짜가 바뀜 -> 초기화 전에 빙고 점수 계산
-//                System.out.println("daily 빙고!, 몇 일??" + logs[i][0]);
-//                for (int j = 0; j < 3; j++) {
-//                    for (int k = 0; k < 3; k++) {
-//                        System.out.print(daily[j][k]);
-//                    }
-//                    System.out.println();
-//                    System.out.println("---");
-//                }
                 for (int j = 0; j < 3; j++) {
                     for (int k = 0; k < 3; k++) {
                         dfs(daily, j, k, "all", 0);
                     }
                 }
-//                System.out.println("몇 점??????????" + bingo);
-                //바뀐 날짜가 기존의 주간 날짜와도 8일이상 차이가 나는지?
-                if (logs[i][0] >= day + 7) {
-//                    System.out.println("주간 빙고 계산 시작!");
+                if (logs[i][0] >= day + 7) { //바뀐 날짜가 7일 이상 차이 난다면 주간 빙고도 계산 후 초기화
                     for (int j = 0; j < 3; j++) {
                         for (int k = 0; k < 3; k++) {
                             dfs(weekly, j, k, "all", 0);
@@ -83,14 +68,6 @@ public class Netmarble2022ct {
             int num = logs[i][1];
             bingoArray(num);
         }
-//        System.out.println("월간 빙고 계산 시작!!");
-//        for (int j = 0; j < 3; j++) {
-//            for (int k = 0; k < 3; k++) {
-//                System.out.print(monthly[j][k]);
-//            }
-//            System.out.println();
-//            System.out.println("---");
-//        }
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 3; k++) {
                 dfs(monthly, j, k, "all", 0);
@@ -100,7 +77,7 @@ public class Netmarble2022ct {
         return bingo;
     }
 
-    public static void bingoArray(int m) {
+    public static void bingoArray(int m) { // logs 배열에 입력된 값들을 일간, 주간, 월간 빙고에 분류 후 입력
         if (1 <= m && m <= 3)
             daily[0][m - 1] = 1;
         else if (4 <= m && m <= 6) daily[1][m - 4] = 1;
@@ -113,7 +90,7 @@ public class Netmarble2022ct {
         else if (25 <= m && m <= 27) monthly[2][m - 25] = 1;
     }
 
-    public static void fillZero(char arr) {
+    public static void fillZero(char arr) { // 날짜가 바뀜으로써 빙고를 초기화시킬 때 사용하는 클래스
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 daily[i][j] = 0;
@@ -124,23 +101,18 @@ public class Netmarble2022ct {
     }
 
     public static void dfs(int[][] board, int row, int col, String dir, int bingoStack) {
-//        System.out.println(row + ", " + col + ", 방향 어디?? " + dir + ", 빙고스택 : " + bingoStack);
         if (row >= board.length || col >= board[row].length || row < 0 || col < 0) {//배열의 범위를 벗어나면 리턴
-//            System.out.println("배열 범위 벗어남. row = " + row + ", col = " + col + ", bingo스택 : " + bingoStack);
-            if (bingoStack >= 3) {
+            if (bingoStack >= 3)
                 bingo++;
-//                System.out.println("bingo 점수 획득. bingo " + bingo);
-            }
             return;
         }
-        if (board[row][col] == 0) return; // 체크 안 된 빙고라면 리텅
+        if (board[row][col] == 0) return; // 체크 안 된 빙고라면 리턴
 
-//        //오른쪽 아래 가는 방향 재귀
-//        int size = (int) board[row][col];
         switch (dir) {
+        //위에서부터 아래로 내려오기 때문에 위로가는 dfs는 할 필요 없음
+        //오른쪽에서 왼쪽으로 가기 때문에 왼쪽으로 가는 dfs도 할 필요 없음.
             case "all":
                 dfs(board, row, col + 1, "right", bingoStack + 1); //오른쪽
-//                dfs(board,row,col-1,"left",bingoStack+1); //왼쪽
                 dfs(board, row + 1, col, "down", bingoStack + 1);//아래
                 if (row == 0 && col == 0) { // 1 번 째 칸일 때 우측하단으로 가는 대각선 빙고도 검색
                     dfs(board, row + 1, col + 1, "rightDown", bingoStack + 1);//우측 아래
@@ -151,9 +123,6 @@ public class Netmarble2022ct {
             case "right":
                 dfs(board, row, col + 1, "right", bingoStack + 1); //오른쪽
                 break;
-//            case "left":
-//                dfs(board,row,col-1,"left",bingoStack+1); //왼쪽
-//                break;
             case "down":
                 dfs(board, row + 1, col, "down", bingoStack + 1);//아래
                 break;
@@ -164,8 +133,5 @@ public class Netmarble2022ct {
                 dfs(board, row + 1, col - 1, "leftDown", bingoStack + 1);//아래
                 break;
         }
-
-        //위에서부터 아래로 내려오기 때문에 위로가는 dfs는 할 필요 없음
-        //오른쪽에서 왼쪽으로 가기 때문에 왼쪽으로 가는 dfs도 할 필요 없음.
     }
 }
